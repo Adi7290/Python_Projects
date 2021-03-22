472c2f57d1f81a1b73de6fd4d7960e0e25895c3c
Aditya_Tundwal <adityatundwal2000@gmail.com>
2021-03-22 23:05:20 +0530

    notes for array method 3

---
+++ array3.py
@@ -0,0 +1,21 @@
+from array import * 
+a = array('i',[1,2,3,4,5,6,7,8,9,])
+print(a)
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[4])
+
+
+#arrays are mutable
+#len is used to get the length of array
+#append is used to add values in array in the end
+#extend adds more than one element in array
+#insert is used to add value on a specific place in array
+#pop is used to delete value from array and return it
+#remove is used to delete value from array and don't return it
+#+ is used to add arrays
+#[:] is used to slice and get the value of array
+#for and while used to loop over an array
+
