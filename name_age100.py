8258bda585d655ffe3a82389a098d3cce275a816
Aditya_Tundwal <adityatundwal2000@gmail.com>
2021-04-02 22:28:26 +0530

    Add files via upload

---
+++ name_age100.py
@@ -0,0 +1,20 @@
+'''Create a program that asks the user to enter their name and their age.
+Print out a message addressed to them that tells them
+the year that they will turn 100 years old.
+
+Extras:
+
+Add on to the previous program by asking the user for another number
+and printing out that many copies of the previous message.
+(Hint: order of operations exists in Python)
+Print out that many copies of the previous message on separate lines.
+(Hint: the string "\n is the same as pressing the ENTER button)'''
+
+
+name = input('Enter Your Name :\t')
+age = int(input('Enter your age :\t'))
+
+years = 100-age
+
+print(f'Hello {name}, you will be a 100 years old in {years} years.')
+
