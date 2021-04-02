3deb1434c21b3d3b64a72fd1d3394d1364e30432
Aditya_Tundwal <adityatundwal2000@gmail.com>
2021-04-02 22:27:48 +0530

    Add files via upload

---
+++ areaofrectangle.py
@@ -0,0 +1,9 @@
+def areaofrectangle():
+    '''To find the area of the rectangle'''
+    length = int(input('Enter the length of the rectangle:\t'))
+    breadth = int(input('Enter the breadth of the triangle:\t'))
+    print(f'The area of your rectangle is :\t {length*breadth}')
+
+
+if __name__ == '__main__':
+    areaofrectangle()
